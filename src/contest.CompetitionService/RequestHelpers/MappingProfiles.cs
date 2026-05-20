386bc991b53578f4f61e1117b01faf22858ce498
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2026-05-20 21:53:58 +0500

    Mapping

---
+++ src/contest.CompetitionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using contest.CompetitionService.DTOs;
+using contest.CompetitionService.Entities;
+
+namespace contest.CompetitionService.RequestHelpers;
+
+public class MappingProfiles: Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Competition, CompetitionDto>();
+        CreateMap<Participant, ParticipantDto>();
+        CreateMap<Venue, VenueDto>();
+    }
+}
